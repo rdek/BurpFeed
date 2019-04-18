f72abf7e142db9fba8552ac9962b5c95af4d52f6
Radosław Maczuga <radoslaw.maczuga@picodi.com>
2019-04-18 10:59:35 +0200

    Added "ignore blank lines" functionality

--- bfeed.py
+++ bfeed.py
@@ -1,4 +1,4 @@
-#!/env/python
+#!/usr/bin/env python
 # Burp URL Feeder Non-Threaded
 # ZephrFish 2018
 import urllib3
@@ -7,28 +7,34 @@ import re
 import requests
 import argparse
 
+urllib3.disable_warnings()
 # urllib3.exceptions.InsecureRequestWarning
 
+def noBlankLines(f):
+    for l in f:
+        line = l.rstrip()
+        if line:
+            yield line
 
 def burpFeed(urls):
-        proxy = {
+    proxy = {
+        "http": "http://127.0.0.1:8080",
+        "https": "https://127.0.0.1:8080",
+    }
 
-                "http": "http://127.0.0.1:8080",
-                "https": "https://127.0.0.1:8080",
-        }
-
-        with open(urls.rstrip(), 'r') as f:
-                for url in f:
-                        regex=re.compile('^http://|^https://')
-                        if re.match(regex, url):
-                                normalresponse = requests.get(url.rstrip(), proxies=proxy, verify=False)
-                                print(url, normalresponse.status_code)
-                        else:
-                                HTTPSecure = "https://"+url.rstrip()
-                                HTTPNot = "http://"+url.rstrip()
-                                httpsresponse = requests.get(HTTPSecure, proxies=proxy, verify=False)
-                                httpresponse = requests.get(HTTPNot, proxies=proxy, verify=False)
-                                print(url.rstrip(), httpsresponse.status_code, httpresponse.status_code)
+    with open(urls.rstrip(), 'r') as f:
+        lines = filter(None, (line.rstrip() for line in f))
+        for url in lines:
+            regex=re.compile('^http://|^https://')
+            if re.match(regex, url):
+                normalresponse = requests.get(url.rstrip(), proxies=proxy, verify=False)
+                print(url, normalresponse.status_code)
+            else:
+                HTTPSecure = "https://"+url.rstrip()
+                HTTPNot = "http://"+url.rstrip()
+                httpsresponse = requests.get(HTTPSecure, proxies=proxy, verify=False)
+                httpresponse = requests.get(HTTPNot, proxies=proxy, verify=False)
+                print(url.rstrip(), httpsresponse.status_code, httpresponse.status_code)
 
 
 if __name__ == '__main__':
